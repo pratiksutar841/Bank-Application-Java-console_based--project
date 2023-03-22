af56b27a8b8bbe6024449da0d7002455a1249b83
Pratik <Pratik@pratik>
2023-03-22 11:06:57 +0530

    my bank commit

---
+++ src/Bank_Application/Main.java
@@ -0,0 +1,12 @@
+package Bank_Application;
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Operation operlation = new Operation();
+		operlation.bankinfo();
+	}
+
+}
